1f3490a80263c898f99e51efabf9ab1635f69dd8
Fshsd <fahdalharbi321@gmail.com>
2025-05-12 00:10:10 +0300

    Script EnemyAI <Empty>

--- Assets/MyScript/EnemyAi.cs
+++ Assets/MyScript/EnemyAi.cs
@@ -3,15 +3,41 @@ using UnityEngine.AI;
 
 public class EnemyAi : MonoBehaviour
 {
-    public NavMeshAgent ag
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround , whatIsPlayer;
+
+    //Partoling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange , attackRange;
+    public bool playerInSightRange , playerInAttackRange;
+
+
+    private void Awake()
+    {
+        player.GetComponent<Transform>();
+        agent.GetComponent<NavMeshAgent>();
+    }
+
+    private void Patroling()
+    {
+
+    }
+    private void ChasePlayer()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void AttackPlayer()
     {
         
     }
