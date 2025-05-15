a48c79c660283d3fe0f30164a39d0341f04057d2
Fshsd <fahdalharbi321@gmail.com>
2025-05-15 08:48:16 +0300

    Update Everthing

--- Assets/MyScript/MainMenu.cs
+++ Assets/MyScript/MainMenu.cs
@@ -1,36 +1,16 @@
-using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public CinemachineCamera MainMenuCam;
-    public CinemachineCamera PlayerCam;
-
-
-    void Start()
-    {
-        PlayerCam.gameObject.SetActive(false);
-        MainMenuCam.gameObject.SetActive(true);
-
-                // التأكد من أن الماوس مرئي ومتاح
-        Cursor.visible = true;  // يجعل الماوس مرئي
-        Cursor.lockState = CursorLockMode.None;  // يحرر الماوس من قفل الحركة
-    }
     public void PlayGame()
     {
-        PlayerCam.gameObject.SetActive(true);
-        MainMenuCam.gameObject.SetActive(false);
-        Cursor.visible = false;
-Cursor.lockState = CursorLockMode.Locked;
-
-Debug.Log("Click Play");
-
+        SceneManager.LoadScene("Map"); // اسم مشهد اللعب
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit Game");
-        Application.Quit(); 
+        Application.Quit(); // يشتغل فقط في النسخة التنفيذية (build)
     }
 }
